272377f9019cc6b613f96cdbf6eeff7d55e8d672
Jefferson Ramos <ramosjeffersonm@gmail.com>
2018-08-02 22:32:37 +0800

    initial commit

---
+++ src/Components/Clock.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "../Styles/clock.css";
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = this.getTime();
+  }
+
+  componentDidMount() {
+    this.setTimer();
+  }
+
+  getTime() {
+    const currentTime = new Date();
+
+    return {
+      hours: currentTime.getHours(),
+      minutes: currentTime.getMinutes(),
+      seconds: currentTime.getSeconds(),
+      ampm: currentTime.getHours() >= 12 ? "PM" : "AM"
+    };
+  }
+
+  setTimer() {
+    clearInterval(this.interval);
+    this.interval = setInterval(this.updateClock.bind(this), 1000);
+
+    // clearTimeout(this.timeout);
+    // this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+  }
+
+  updateClock() {
+    this.setState(this.getTime);
+    // this.setState(this.getTime, this.setTimer);
+  }
+
+  render() {
+    const { hours, minutes, seconds, ampm } = this.state;
+
+    return (
+      <div className="clock">
+        {hours === 0 ? 12 : hours > 12 ? hours - 12 : hours} :{" "}
+        {minutes > 9 ? minutes : `0${minutes}`} :{" "}
+        {seconds > 9 ? seconds : `0${seconds}`} {ampm}
+      </div>
+    );
+  }
+}
+
+export default Clock;
